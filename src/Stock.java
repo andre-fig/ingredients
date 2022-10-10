b2bf6963030a1b9fc262778551b94f459a7e1f7c
André Figueiredo <afigueiredo@tasken.com>
2022-10-10 20:20:43 -0300

    refactor(#3): Using array of ingredients in stock

--- src/Stock.java
+++ src/Stock.java
@@ -2,34 +2,39 @@
 import java.util.ArrayList;
 
 public class Stock {
-    private ArrayList<String> ingredients;
+    private ArrayList<Ingredient> ingredients;
 
     public Stock() {
-        ingredients = new ArrayList<>();
-        ingredients.add("strawberry");
-        ingredients.add("banana");
-        ingredients.add("pineapple");
-        ingredients.add("mango");
-        ingredients.add("peach");
-        ingredients.add("honey");
-        ingredients.add("blackberry");
-        ingredients.add("blueberry");
-        ingredients.add("black currant");
-        ingredients.add("grape juice");
-        ingredients.add("frozen yogurt");
-        ingredients.add("green apple");
-        ingredients.add("lime");
-        ingredients.add("avocado");
-        ingredients.add("spinach");
-        ingredients.add("ice");
-        ingredients.add("apple juice");
-        ingredients.add("ice cream");
-        ingredients.add("chocolate");
-        ingredients.add("peanut");
-        ingredients.add("cherry");
+        this.ingredients = new ArrayList<>();
+        this.ingredients.add(new Ingredient("strawberry"));
+        this.ingredients.add(new Ingredient("banana"));
+        this.ingredients.add(new Ingredient("pineapple"));
+        this.ingredients.add(new Ingredient("Coconut"));
+        this.ingredients.add(new Ingredient("mango"));
+        this.ingredients.add(new Ingredient("peach"));
+        this.ingredients.add(new Ingredient("honey"));
+        this.ingredients.add(new Ingredient("blackberry"));
+        this.ingredients.add(new Ingredient("black currant"));
+        this.ingredients.add(new Ingredient("grape juice"));
+        this.ingredients.add(new Ingredient("frozen yogurt")); //
+        this.ingredients.add(new Ingredient("green apple"));
+        this.ingredients.add(new Ingredient("lime"));
+        this.ingredients.add(new Ingredient("avocado"));
+        this.ingredients.add(new Ingredient("spinach"));
+        this.ingredients.add(new Ingredient("ice"));
+        this.ingredients.add(new Ingredient("apple juice"));
+        this.ingredients.add(new Ingredient("ice cream"));
+        this.ingredients.add(new Ingredient("chocolate"));
+        this.ingredients.add(new Ingredient("peanut"));
+        this.ingredients.add(new Ingredient("cherry"));
     }
 
-    public boolean hasIngredient(String ingredient) {
-        return ingredients.contains(ingredient);
-    }    
+    public boolean hasIngredient(String name) {
+        for (Ingredient ingredient : this.ingredients) {
+            if (ingredient.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
